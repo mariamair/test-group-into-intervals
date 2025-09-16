657588640a16219150318b4e36dc2ff2c754581b
Maria Mair <mm225mz@student.lnu.se>
2025-09-16 21:23:34 +0200

    Create markdown file from Jest output

--- src/index.js
+++ src/index.js
@@ -4,3 +4,57 @@
  * @author Maria Mair <mm225mz@stundent.lnu.se>
  * @version 0.0.1
  */
+
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs/promises'
+
+console.log('index.js')
+
+const directoryFullName = dirname(fileURLToPath(import.meta.url))
+const inputPath = join(directoryFullName, '..', 'reports', 'report.json')
+const outputPath = join(directoryFullName, '..', 'reports', 'report.md')
+
+console.log(inputPath)
+
+readTestReport()
+
+async function readTestReport () {
+  const result = await fs.readFile(inputPath, 'utf8')
+
+  if (!result) {
+    const error = new Error('Could not read file')
+    throw error
+  }
+
+  const data = await JSON.parse(result)
+  return data
+}
+
+const report = await readTestReport()
+const time = new Date(report.startTime).toString()
+const tableColumns = '| Test | Status |\n |---|---|\n'
+
+let markdownReport = '# Test results\n'
+markdownReport += '**Time performed:** ' + time + '\n\n'
+for (const suite of report.testResults) {
+  markdownReport += '## ' + suite.assertionResults[0].ancestorTitles + '\n\n'
+  markdownReport += tableColumns
+  for (const test of suite.assertionResults) {
+    const status = test.status === 'passed' ? '✅' : '❌'
+    markdownReport += '| ' + test.title + ' | ' + status + ' |\n'
+  }
+}
+
+console.log(markdownReport)
+
+async function writeTestReportToMarkdown () {
+  try {
+    const result = await fs.writeFile(outputPath, markdownReport)
+  } catch (error) {
+    error.text = new Error('Could not read file')
+    throw error
+  }
+}
+
+writeTestReportToMarkdown()
